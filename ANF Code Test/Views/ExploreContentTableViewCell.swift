d4334429d615288f43350b2934d993c1ab3f35f2
Jerry Baez <jerry@atomicrobot.com>
2024-12-12 10:07:10 -0600

    Update the table view cell's to contain the same views as in `DetailsViewController`, and update the app from a "List > Detail" UI layout to a "Product Card" layout.

--- ANF Code Test/Views/ExploreContentTableViewCell.swift
+++ ANF Code Test/Views/ExploreContentTableViewCell.swift
@@ -9,8 +9,13 @@ class ExploreContentTableViewCell: UITableViewCell {
 
     // MARK: IBOutlets
 
+    @IBOutlet private weak var bgImageView: UIImageView!
+    @IBOutlet private weak var topDescLabel: UILabel!
     @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var contentImageView: UIImageView!
+    @IBOutlet private weak var promoLabel: UILabel!
+    @IBOutlet private weak var bottomDescTextView: UITextView!
+    @IBOutlet private weak var descTextStackView: UIStackView!
+    @IBOutlet private weak var buttonsStackView: UIStackView!
 
     // MARK: Vars and Constants
 
@@ -23,10 +28,38 @@ class ExploreContentTableViewCell: UITableViewCell {
         }
     }
 
+    private var contentButtons: [ContentButton] = []
+
+    private var bottomDescAttributedString: NSAttributedString? {
+        guard let exploreItem = self.exploreItem,
+              let bottomDescription = exploreItem.bottomDescription,
+              let htmlData = bottomDescription.data(using: .unicode),
+              let attributedText = try? NSMutableAttributedString(
+                data: htmlData,
+                options: [.documentType: NSAttributedString.DocumentType.html],
+                documentAttributes: nil)
+        else {
+            return nil
+        }
+
+        let range = NSRange(location: 0, length: attributedText.length)
+
+        attributedText.addAttributes(bottomDescriptionAttributes, range: range)
+
+        return attributedText
+    }
+
+    private var bottomDescriptionAttributes: [NSAttributedString.Key: Any] {
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.alignment = .center
+
+        return [.paragraphStyle: paragraph, .font: UIFont.systemFont(ofSize: 13), .foregroundColor: UIColor.systemGray3]
+    }
+
     // NOTE: The following computed vars are for testing purposes only:
 
     var title: String? { titleLabel.text }
-    var image: UIImage? { contentImageView.image }
+    var image: UIImage? { bgImageView.image }
 
     // MARK: UITableViewCell Lifecycle
 
@@ -43,26 +76,96 @@ class ExploreContentTableViewCell: UITableViewCell {
             resetViewsContent()
             return
         }
+        
+        setupBackgroundImage()
 
+        topDescLabel.text = exploreItem.topDescription
         titleLabel.text = exploreItem.title
+        promoLabel.text = exploreItem.promoMessage
+
+        generateContentButtons()
+
+        guard let attributedText = bottomDescAttributedString else {
+            bottomDescTextView.text = nil
+            bottomDescTextView.attributedText = nil
+            descTextStackView.isHidden = true
+
+            return
+        }
+
+        // Setup Text View link attributes:
+        bottomDescTextView.linkTextAttributes = bottomDescriptionAttributes
+
+        descTextStackView.isHidden = false
+        bottomDescTextView.attributedText = attributedText
+    }
+
+    private func setupBackgroundImage() {
+        guard let exploreItem else { return }
 
         guard let backgroundImageUrl = exploreItem.backgroundImageUrl else {
             return
         }
 
+        // Re-attempt to fetch the image from the web.
+        // The URLSession instance uses the `NSURLRequestUseProtocolCachePolicy` as default.
+        // Therefore, if this image has been already downloaded (at the previous view controller),
+        // the request should just retrieve the cached image from the local storage,
+        // and complete the request successfully:
+
         Task {
             do {
                 let image = try await NetworkManager.shared.downloadImage(url: backgroundImageUrl)
-                contentImageView.image = image
+
+                bgImageView.isHidden = false
+                bgImageView.image = image
             } catch {
+                bgImageView.isHidden = true
                 print("Error: \(error.localizedDescription)")
             }
         }
     }
 
-    private func resetViewsContent() {
-        titleLabel.text = nil
-        contentImageView.image = nil
+    private func generateContentButtons() {
+        guard let contentList = exploreItem?.content else {
+            buttonsStackView.isHidden = true
+            return
+        }
+
+        buttonsStackView.isHidden = false
+
+        // Remove any existing buttons on the stack view:
+        buttonsStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+
+        contentList.forEach {
+
+            let button = ContentButton()
+            button.setTitle($0.title, for: .normal)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+            buttonsStackView.addArrangedSubview(button)
+            contentButtons.append(button)
+
+            guard let url = $0.targetUrl, UIApplication.shared.canOpenURL(url) else {
+                return
+            }
+
+            let action = UIAction() { _ in
+                UIApplication.shared.open(url)
+            }
+
+            button.addAction(action, for: .touchUpInside)
+        }
     }
 
+    private func resetViewsContent() {
+        bgImageView.image = nil
+        topDescLabel.text = nil
+        titleLabel.text = nil
+        promoLabel.text = nil
+        bottomDescTextView.text = nil
+
+        contentButtons.forEach { $0.removeFromSuperview() }
+    }
 }
